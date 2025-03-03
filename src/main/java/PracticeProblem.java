932cdbdb2324126e644d6afbdb74be2c5d1cea6b
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-03 00:56:07 +0000

    review 15 compelete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,6 +2,24 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+
+	}
+	public static int goodString(String word){
+		String[] wordArr=word.split("");
+		int remove = wordArr.length;
+		int tempremove= 0;
+		for (int i=0; i<wordArr.length-1; i++){
+			if (word.indexOf(wordArr[i])!= word.lastIndexOf(wordArr[i])){
+				tempremove = wordArr.length-(word.lastIndexOf(wordArr[i])-word.indexOf(wordArr[i])+1);
+				if (tempremove<remove){
+					remove=tempremove;
+				}
+			}
+		}
+		if (remove == wordArr.length){
+			remove=-1;
+		}
+		return remove;
 	}
 
 }
